f01eb48f5dca3052ebf34f1dde180ca7bad8461c
Macedo <pedromacedo113@hotmail.com>
2022-07-30 23:09:38 -0300

    Starting project

---
+++ src/main/java/com/mcd/digitalbank/DigitalbankApplication.java
@@ -0,0 +1,13 @@
+package com.mcd.digitalbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcd/digitalbank/DigitalbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcd.digitalbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
